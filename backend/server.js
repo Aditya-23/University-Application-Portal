3c3eac374d97bf796a5523d478555a817f21e5b7
Aditya Mysore <aditya@Adityas-MacBook-Air.local>
2022-11-11 17:40:28 -0500

    feat: Added a provision for the app to run on different port

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ import cors from "cors";
 import express from "express";
 import MongoConnect from "./config/mongodb.js";
 import routes from "./routes/index.js";
+import config from "config";
 
 const app = express();
+const PORT = config.get("port");
 
 app.use(express.json());
 app.use(cors());
@@ -11,6 +13,6 @@ MongoConnect();
 
 routes(app);
 
-app.listen(8000, () => {
-    console.log("App is running on 8000")
+app.listen(PORT, () => {
+    console.log(`App is running on ${PORT}`)
 });
