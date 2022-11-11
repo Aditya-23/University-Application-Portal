61f15da360d39ce282226c2ee847e26ec294a745
Aditya Mysore <aditya@Adityas-MacBook-Air.local>
2022-11-11 17:33:06 -0500

    feat: Added provision to connect to mongodb atlas

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import cors from "cors";
 import express from "express";
+import MongoConnect from "./config/mongodb.js";
 import routes from "./routes/index.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+MongoConnect();
 
 routes(app);
 
