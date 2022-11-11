19f6f128be893d296bc5eb745bf5421c09b07d23
Aditya Mysore <aditya@Adityas-MacBook-Air.local>
2022-11-11 17:13:25 -0500

    feat: defined main function in the routes/index.js that has all the main routes

---
+++ backend/routes/index.js
@@ -0,0 +1,6 @@
+//Import all the routes here
+
+const mainRouter = (app) => {
+    //write all your imported routes here
+}
+export default mainRouter;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
-const cors = require("cors");
-const express = require("express");
+import cors from "cors";
+import express from "express";
+import routes from "./routes/index.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors);
+
+routes(app);
 
 app.listen(8000, () => {
     console.log("App is running on 8000")
