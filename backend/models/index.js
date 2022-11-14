738ce49c30125e93133def06188da0e5c05ff720
avaneesh-neu <yembadi.a@northeastern.edu>
2022-11-14 11:59:59 -0500

    feat: add student model

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,2 +1,4 @@
 // Import all the models here and then export them
 // Create a file for each model and export the created model from them
+
+import Student from "./student.js";
---
+++ backend/models/student.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const studentSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  education: {
+    type: Array,
+    required: false,
+  },
+  experience: {
+    type: Array,
+    required: false,
+  },
+  greScore: {
+    type: Number,
+    required: false,
+  },
+  toeflScore: {
+    type: Number,
+    required: false,
+  },
+  ieltsScore: {
+    type: Number,
+    required: false,
+  },
+  governmentId: {
+    type: String,
+    required: false,
+  },
+});
+
+const Student = mongoose.model("Student", studentSchema);
+
+export default Student;
