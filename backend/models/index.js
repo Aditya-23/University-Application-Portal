bd4a141819a218d7db686aaeab0025cade1b3d95
aditya-ramesh-mysore <mysore.ad@northeastern.edu>
2022-11-15 18:22:37 -0500

    chore: Included all the models in the models/index.js file

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,7 +1,17 @@
 // Import all the models here and then export them
 import UserAdmin from "./UserAdmin";
 import Student from "./student.js";
+import Education from "./education";
+import Application from "./application";
+import University from "./university";
+import Program from "./program";
 // Create a file for each model and export the created model from them
 
-export { Student,
-    UserAdmin };
+export { 
+    Student,
+    UserAdmin,
+    Education,
+    Application,
+    University,
+    Program, 
+};
