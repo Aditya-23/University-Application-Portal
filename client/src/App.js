1af0dcf091f0d6295e1c53af22c2918def1ee291
aditya-ramesh-mysore <mysore.ad@northeastern.edu>
2022-11-25 17:41:13 -0500

    feat: Added basic components and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {Provider} from 'react-redux';
+import store from "./store";
+import Landing from './components/Landing';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Layout from './components/Layout';
+import Login from './components/Login';
+import Navbar from './components/Navbar';
+import Register from './components/Register';
+import Dashboard from './components/Dashboard';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Provider store={store}>
+            <Navbar/>
+            <BrowserRouter>
+                <Routes>
+                    <Route exact path='/' element={<Landing/>}/>
+                    <Route exact path='/login' element={< Login />}/>
+                    <Route exact path='/register' element={<Register/>}/>
+                    <Route exact path='/dashboard' element={<Dashboard/>} />
+                </Routes>
+            </BrowserRouter>
+        </Provider>
+
+    );
 }
 
 export default App;
---
+++ client/src/components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+export default function Login() {
+  return (
+    <>
+        
+        <h1>Login</h1>
+    </>
+  );
+}
