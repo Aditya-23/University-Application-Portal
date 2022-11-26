d43f87c2216244dddc346a391d867d3dbac9fc78
avaneesh-neu <yembadi.a@northeastern.edu>
2022-11-25 22:14:20 -0500

    feat: add model to store experiences

---
+++ backend/models/experience.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const experienceSchema = new mongoose.Schema({
+  jobTitle: {
+    type: String,
+    required: true,
+  },
+  companyName: {
+    type: String,
+    required: true,
+  },
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    required: false,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+const Experience = mongoose.model("Experience", experienceSchema);
+
+export default Experience;
