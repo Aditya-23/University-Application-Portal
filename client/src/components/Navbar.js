51043984bf475e5cd7fa5b481804666251a552c0
aditya-ramesh-mysore <mysore.ad@northeastern.edu>
2022-11-26 16:52:50 -0500

    feat: navbar added

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
-export default function Navbar() {
-  return (
-    <>
-        <h1>Navbar</h1>
-    </>
-  );
+function Navbar(props) {
+
+    return (
+        <h1>
+          Navbar
+        </h1>
+    )
 }
+
+const mapStateToProps = state => {
+    return {auth: state.authReducer}
+}
+
+export default connect(mapStateToProps, null)(Navbar);
