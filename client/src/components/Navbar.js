199a6bb6f1afeb68512a20468cbae7679debb26d
aditya-ramesh-mysore <mysore.ad@northeastern.edu>
2022-11-26 19:48:15 -0500

    feat: login, logout registration navbar done

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,28 +1,30 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {logoutUser} from '../actions/auth';
 
 function Navbar(props) {
 
-  const onLogout = () => {
-
-  }
-
+    const onLogout = async() => {
+        await props.logoutUser();
+    }
     return (
-        <div class="topnav">
+      <>
             {props.auth.isAuthenticated
-                ? <> <div onClick={() => onLogout()}>
-                      <h1>Logout</h1>
-                    </div> 
-                <form className = 'search_bar_wrap' > <input
+                ? <div className='topnav'> <div className='logo_styles'>
+                    <img src={require("../static/logo.ico")} className="img_style" />
+                    <p className="name_style">University Application</p>
+                </div> < form className = 'search_bar_wrap' > <input
                     className='search_bar'
                     type="text"
                     placeholder='Search University'
-                    size="40"/> < input className = 'search_bar' type = "submit" value = "Search" /> </form> < a > Logout </a>
-                </>
-                : <> <a href='/login'>Login</a> < a href = '/register' > SignUp </a>
-            </ >
-            }
-        </div>
+                    size="40"/> < input className = 'search_bar' type = "submit" value = "Search" /> </form> < a onClick = {
+                    () => onLogout()
+                } > Logout </a>
+                </div>
+                : <div className='log_out_nav'> <a href='/login' className='login_style'>Login</a> < a href = '/register' className='sign_up_style'> SignUp </a>
+            </div>
+}
+</>
     )
 }
 
@@ -30,4 +32,4 @@ const mapStateToProps = state => {
     return {auth: state.authReducer}
 }
 
-export default connect(mapStateToProps, null)(Navbar);
+export default connect(mapStateToProps, {logoutUser})(Navbar);
