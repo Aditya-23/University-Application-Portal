f1f67d15a016dc21654b9b65e5de1493da852523
avaneesh-neu <yembadi.a@northeastern.edu>
2022-11-30 12:30:19 -0500

    commit Dashboard before merge

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { connect } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import ApplicationSection from "./ApplicationSection.js";
+import UniversitySection from "./TopUniversitySection.js";
 
 function Dashboard(props) {
-    if(props.auth.loading){
-        return (
-            <h1>Loading</h1>
-        )
-    }
-    return ( <> <h1>Dashboard</h1> </>
+  if (props.auth.loading) {
+    return <h1>Loading</h1>;
+  }
+  return (
+    <>
+      {" "}
+      <h1>Dashboard</h1>
+      <UniversitySection />
+      <ApplicationSection />
+    </>
   );
 }
 
-const mapStateToProps = state => {
-    return {
-        auth: state.authReducer,
-    }
-}
+const mapStateToProps = (state) => {
+  return {
+    auth: state.authReducer,
+  };
+};
 
-
-export default connect(mapStateToProps, null) (Dashboard);
+export default connect(mapStateToProps, null)(Dashboard);
