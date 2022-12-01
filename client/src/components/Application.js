58f190bf078efa90b72c641e1b167effc897c0c7
aditya-ramesh-mysore <mysore.ad@northeastern.edu>
2022-12-01 00:01:20 -0500

    feat: adding form elements to the application form

--- client/src/components/Application.js
+++ client/src/components/Application.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import {Button, Col, Form, Row} from 'react-bootstrap';
 
 export default function Application() {
-  return (
-    <>
-        <h1>Application</h1>
-    </>
-  );
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log("INVOKED HERERREE")
+  }
+    return (
+        <div className='application-container'>
+            <Form onSubmit={(e) => onSubmitHandler(e)}>
+              <Form.Label><h3>Personal Information</h3></Form.Label>
+                <Form.Group className="mb-3" >
+                    <Form.Label>Full Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter full name as per your passport"/>
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" placeholder="Enter your personal email"/>
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" >
+                    <Form.Label>Full Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter full name as per your passport"/>
+                </Form.Group>
+
+
+                <Form.Group className="mb-3">
+                    <Form.Label>Applying to
+                    </Form.Label>
+                    <Form.Control type="text" value={"Northeastern"} disabled/>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Semester Intake -
+                    </Form.Label>
+                    <Form.Control type="email" placeholder="Enter email"/>
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </div>
+    );
 }
