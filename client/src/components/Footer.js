71b9764a609e67c11bdb5b17d37eec801aad093f
Swapnil Patil <swapnilpatil@Swapnils-MacBook-Air.local>
2022-12-02 16:03:39 -0500

    Completed Footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,22 @@ import React from 'react';
 export default function Footer() {
   return (
     <>
-        <footer>Footer</footer>
+        <footer className='container-footer'>
+          <div className='footer-main'>
+            <h4> University Name </h4>
+          <div className='myDivElement'>
+          <ul>
+               <li><a href="#home">Home</a></li>
+               <li><a href="#about">About Us</a></li>
+               <li><a href="#clients">Our Clients</a></li>  
+               <li><a href="#contact">Contact Us</a></li>
+          </ul>
+          </div>
+          <div className='copyright'>
+              <p>©2022 University | 390 Huntington Avenue, Boston, MA </p>
+          </div>
+          </div>
+        </footer>
     </>
   );
 }
