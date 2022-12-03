db6dbc0c5c03119f9531240d47526883827cc410
avaneesh-neu <yembadi.a@northeastern.edu>
2022-12-03 16:45:16 -0500

    feat: add navigate to uni on click of card

--- client/src/components/TopUniversitySection.js
+++ client/src/components/TopUniversitySection.js
@@ -1,16 +1,28 @@
 import "../styles/main.scss";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import { connect } from "react-redux";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import { startUniversityLoad } from "../actions/universities";
 import { useGetUniversitiesQuery } from "../api/uniTemp.js";
 
 function UniversityCard(props) {
+  const navigate = useNavigate();
+  const uniLoadHandler = () => {
+    console.log("uniLoadHandler", props.id);
+    props.startUniversityLoad(props.id);
+    navigate("/university");
+  };
+
   var card = (
     <li className="cards__item">
       <Card bg="Primary">
         {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
         <Card.Body>
-          <Card.Title> {props.name} </Card.Title>
-          <Card.Text>{props.description}</Card.Text>
+          <div onClick={() => uniLoadHandler()}>
+            <Card.Title> {props.name} </Card.Title>
+            <Card.Text>{props.description}</Card.Text>
+          </div>
           <Button variant="primary">Shortlist Uni</Button>
         </Card.Body>
       </Card>
@@ -19,7 +31,7 @@ function UniversityCard(props) {
   return card;
 }
 
-function UniversitySection() {
+function UniversitySection(props) {
   const {
     data: Universities,
     isLoading,
@@ -35,8 +47,10 @@ function UniversitySection() {
     items = Universities.map((uni) => (
       <UniversityCard
         key={uni._id}
+        id={uni._id}
         name={uni.name}
         description={uni.description}
+        startUniversityLoad={props.startUniversityLoad}
       />
     ));
   } else if (isError) {
@@ -50,5 +64,14 @@ function UniversitySection() {
     </div>
   );
 }
+const mapStateToProps = (state) => {
+  return {
+    auth: state.authReducer,
+  };
+};
 
-export default UniversitySection;
+export default connect(mapStateToProps, { startUniversityLoad })(
+  UniversitySection
+);
+
+// export default UniversitySection;
