c8664984b75bf1c1af94c6be2dca1b1d598bb3eb
avaneesh-neu <yembadi.a@northeastern.edu>
2022-12-03 17:31:27 -0500

    chore: refactor Unicard into it's file for sharing

--- client/src/components/dashboard/TopUniversitySection.js
+++ client/src/components/dashboard/TopUniversitySection.js
@@ -1,35 +1,8 @@
 import "../../styles/main.scss";
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
 import { connect } from "react-redux";
-import { Link, Navigate, useNavigate } from "react-router-dom";
 import { startUniversityLoad } from "../../actions/universities";
+import UniversityCard from "./UniCard.js";
 import { useGetUniversitiesQuery } from "../../api/uniTemp.js";
-
-function UniversityCard(props) {
-  const navigate = useNavigate();
-  const uniLoadHandler = () => {
-    console.log("uniLoadHandler", props.id);
-    props.startUniversityLoad(props.id);
-    navigate("/university");
-  };
-
-  var card = (
-    <li className="cards__item">
-      <Card bg="Primary">
-        {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-        <Card.Body>
-          <div onClick={() => uniLoadHandler()}>
-            <Card.Title> {props.name} </Card.Title>
-            <Card.Text>{props.description}</Card.Text>
-          </div>
-          <Button variant="primary">Shortlist Uni</Button>
-        </Card.Body>
-      </Card>
-    </li>
-  );
-  return card;
-}
 
 function UniversitySection(props) {
   const {
