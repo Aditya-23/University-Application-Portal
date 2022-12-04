2d860f31a5217419632f7a42e3be4ec102c26724
avaneesh-neu <yembadi.a@northeastern.edu>
2022-12-04 09:30:23 -0500

    feat: add fetch & filter on Applications

--- client/src/components/dashboard/ApplicationSection.js
+++ client/src/components/dashboard/ApplicationSection.js
@@ -1,9 +1,96 @@
+import { applyPatches } from "immer";
+import { useEffect, useState } from "react";
+import { useGetApplicationsByStudentIdQuery } from "../../api/applicationApi";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+
+const AppplicationStatus = ["Pending", "In Review", "Accepted", "Rejected"];
+
+function ApplicationCard({ Application }) {
+    return (
+        <div>
+            {Application._id}
+            {Application.applicationStatus}
+        </div>
+    );
+}
+
+function ApplicationSelector({ statusFilter, setStatusFilter }) {
+    // refresh Section when statusFilter changes
+    useEffect(() => {}, [statusFilter]);
+    function handleSelect(e) {
+        console.log(e);
+        setStatusFilter(e);
+    }
+
+    var selectorContent = AppplicationStatus.map(status => (
+        <Dropdown.Item key={status} eventKey={status}>
+            {status}
+        </Dropdown.Item>
+    ));
+
+    return (
+        <div>
+            {/* <p>Filter Applications:</p> */}
+            <DropdownButton
+                id="dropdown-basic-button"
+                onSelect={handleSelect}
+                title={statusFilter}
+            >
+                <Dropdown.Menu>
+                    <Dropdown.Item key="All" eventKey="All">
+                        All
+                    </Dropdown.Item>
+                    {selectorContent}
+                </Dropdown.Menu>
+            </DropdownButton>
+        </div>
+    );
+}
+
 function ApplicationSection() {
-  return (
-    <div>
-      <h2>Applications</h2>
-    </div>
-  );
+    const {
+        data: Applications,
+        isLoading,
+        isSuccess,
+        isError,
+        error,
+    } = useGetApplicationsByStudentIdQuery("6382996e0b0d5cc4eccec77c");
+    // var statusFilter = "all";
+    const [statusFilter, setStatusFilter] = useState("All");
+
+    var items = [];
+    // refresh Section when statusFilter changes
+    useEffect(() => {}, [statusFilter]);
+    if (isLoading) {
+        items = <p>Loading...</p>;
+    } else if (isSuccess) {
+        if (AppplicationStatus.includes(statusFilter)) {
+            items = Applications.filter(
+                Application => Application.applicationStatus === statusFilter
+            ).map(Application => (
+                <ApplicationCard key={Application._id} Application={Application} />
+            ));
+        } else {
+            items = Applications.map(Application => (
+                <ApplicationCard key={Application._id} Application={Application} />
+            ));
+        }
+    } else if (isError) {
+        items = <p>Loading</p>;
+    }
+
+    return (
+        <div>
+            <h2>Applications</h2>
+            <ApplicationSelector
+                statusFilter={statusFilter}
+                setStatusFilter={setStatusFilter}
+            />
+            <p>{statusFilter}</p>
+            <ul>{items}</ul>
+        </div>
+    );
 }
 
 export default ApplicationSection;
