30ea500c2e3c300b227a4b677d209c6d764c507d
avaneesh-neu <yembadi.a@northeastern.edu>
2022-12-04 12:44:06 -0500

    fix: clean unrequired text & minor styling changes

--- client/src/components/dashboard/ShortlistSection.js
+++ client/src/components/dashboard/ShortlistSection.js
@@ -43,7 +43,6 @@ function ShortlistSection(props) {
         <div>
             <h3>Shortlist Section</h3>
             <h2 className="title">Top Universities</h2>
-            <p>{shortlistedIds.join(", ")}</p>
             <ul className="cardsList">{items}</ul>
         </div>
     );
