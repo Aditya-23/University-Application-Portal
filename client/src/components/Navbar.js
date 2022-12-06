f4e8548ec3cbbf8621ab4f19ea55115e7c3e5ace
Swapnil-Narendra-Patil <patil.swap@northeastern.edu>
2022-12-06 17:37:49 -0500

    updated Searchbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,46 +1,52 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import {logoutUser} from '../actions/auth';
+import React from "react";
+import { connect } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { logoutUser } from "../actions/auth";
 
 function Navbar(props) {
+  const onLogout = () => {
+    props.logoutUser();
+  };
 
-
-    const onLogout = () => {
-        props.logoutUser();
-    }
-
-
-    return (
-      <>
-            {props.auth.isAuthenticated
-                ? <div className='topnav'> <div className='logo_styles'>
-                    <img src={require("../static/uniicon.ico")} className="img_style" />
-                    <p className="name_style">University Application</p>
-                </div> <form className = 'search_bar_wrap' > <input
-                    className='search_bar'
-                    type="text"
-                    placeholder='Search University'
-                    size="40"/> < input className = 'search_bar' type = "submit" value = "Search" /> </form>
-                    <a onClick = {() => onLogout()} > Logout </a>
-                </div>
-                : <div className='topnav'>
-                  <div className='logo_styles'>
-                    <img src={require("../static/uniicon.ico")} className="img_style" />
-                    <p className="name_style">University Application</p>
-                  </div>
-                    <div className='log_out_nav'> 
-                      <a href='/login' className='login_style'>Login</a> 
-                      <a href = '/register' className='sign_up_style'> SignUp </a>
-                    </div>
-                  </div>
-              }
-</>
-    )
+  return (
+    <>
+      {props.auth.isAuthenticated ? (
+            <><link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous" /><link rel="stylesheet" href="./style.css" /><div className="topnav">
+          {" "}
+          <div className="logo_styles">
+            <img src={require("../static/uniicon.ico")} className="img_style" />
+            <p className="name_style">University Application</p>
+          </div>{" "}
+          <form className="search_bar_wrap">
+            {" "}
+            <input id="searchBar" class="searchbar" type="text" placeholder="Search..."/>
+  <a id="btnSearch" class="btn-search"><i class="fa fa-search"></i></a>{" "}
+          </form>
+          <a onClick={() => onLogout()}> Logout </a>
+        </div></>
+      ) : (
+        <div className="topnav">
+          <div className="logo_styles">
+            <img src={require("../static/uniicon.ico")} className="img_style" />
+            <p className="name_style">University Application</p>
+          </div>
+          <div className="log_out_nav">
+            <a href="/login" className="login_style">
+              Login
+            </a>
+            <a href="/register" className="sign_up_style">
+              {" "}
+              SignUp{" "}
+            </a>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
-const mapStateToProps = state => {
-    return {auth: state.authReducer}
-}
+const mapStateToProps = (state) => {
+  return { auth: state.authReducer };
+};
 
-export default connect(mapStateToProps, {logoutUser})(Navbar);
+export default connect(mapStateToProps, { logoutUser })(Navbar);
