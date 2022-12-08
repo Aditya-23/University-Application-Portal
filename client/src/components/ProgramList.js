27f9856239bf1aaa5a2161a9e4cfe34b5eede472
aditya-ramesh-mysore <mysore.ad@northeastern.edu>
2022-12-07 20:00:53 -0500

    showing program details as modal

--- client/src/components/ProgramList.js
+++ client/src/components/ProgramList.js
@@ -2,11 +2,25 @@ import React from 'react';
 import { ListGroup } from 'react-bootstrap';
 
 export default function ProgramList(props) {
+
+    const [hidden, setHidden] = useState(true);
+    const [currentProgram, setCurrentProgram] = useState({
+        courseName: "",
+        credits: "",
+        specializations: [],
+        description: ""
+    });
+
+    const showDetails = (program) => {
+        setCurrentProgram(program);
+        setHidden(false);
+    }
+
     return (
         <ListGroup as="ol" numbered >
             {props
                 .programs
-                .map(program => <ListGroup.Item as="li" >{program.courseName}</ListGroup.Item>)}
+                .map(program => <ListGroup.Item onClick={() => showDetails(program)} as="li" >{program.courseName}</ListGroup.Item>)}
 
         </ListGroup>
     );
