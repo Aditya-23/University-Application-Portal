5312b7bafa72fd9789c98c827a0fa6773e6a086e
avaneesh-neu <yembadi.a@northeastern.edu>
2022-12-07 23:39:48 -0500

    fix: add  update Application

--- client-admin/src/redux/api/applicationApi.js
+++ client-admin/src/redux/api/applicationApi.js
@@ -6,10 +6,32 @@ export const applicationApiSlice = apiSlice.injectEndpoints({
         // GET /applications/?studentId]][]=studentId
         getApplicationsByUniId: builder.query({
             query: UniId => `applications/?universityId=${UniId}`,
-            providesTags: ["Applications"],
-            refetchOnWindowFocus: true,
+            providesTags: result =>
+                // is result available?
+                result
+                    ? // successful query
+                      [
+                          ...result.map(({ id }) => ({ type: "Applications", id })),
+                          { type: "Applications", id: "LIST" },
+                      ]
+                    : // an error occurred, but we still want to refetch this query when `{ type: 'Applications', id: 'LIST' }` is invalidated
+                      [{ type: "Applications", id: "LIST" }],
+        }),
+        updateApplication: builder.mutation({
+            query(data) {
+                const { _id, ...body } = data;
+                return {
+                    url: `applications/${_id}`,
+                    method: "PUT",
+                    body,
+                };
+            },
+            // Invalidates all queries that subscribe to this Post `id` only.
+            // In this case, `getPost` will be re-run. `getPosts` *might*  rerun, if this id was under its results.
+            invalidatesTags: (result, error, { id }) => [{ type: "Applications", id }],
         }),
     }),
 });
 
-export const { useGetApplicationsByUniIdQuery } = applicationApiSlice;
+export const { useGetApplicationsByUniIdQuery, useUpdateApplicationMutation } =
+    applicationApiSlice;
